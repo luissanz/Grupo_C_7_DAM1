0f2ecec6175a6e974e8ed6a3dbbd04ea12b6e51e
ibimarine <rauldoblasespinosa@gmail.com>
2014-05-29 01:42:37 +0200

    bloc de notas
    bloc de notas BETA sin ( la opcion "abrir")

--- ProyectoGrupo7/ProyectoGrupo7/Raul.Designer.cs
+++ ProyectoGrupo7/ProyectoGrupo7/Raul.Designer.cs
@@ -30,6 +30,23 @@
         {
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regalitoDeLaCasaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ganadorDeLaLigaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ganadorDeLaChampionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // radioButton1
@@ -60,6 +77,102 @@
             this.radioButton2.UseVisualStyleBackColor = false;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.formatosToolStripMenuItem,
+            this.regalitoDeLaCasaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(657, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.cerrarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.cerrarToolStripMenuItem.Text = "Cerrar";
+            this.cerrarToolStripMenuItem.Click += new System.EventHandler(this.cerrarToolStripMenuItem_Click);
+            // 
+            // formatosToolStripMenuItem
+            // 
+            this.formatosToolStripMenuItem.Name = "formatosToolStripMenuItem";
+            this.formatosToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.formatosToolStripMenuItem.Text = "Formatos";
+            // 
+            // regalitoDeLaCasaToolStripMenuItem
+            // 
+            this.regalitoDeLaCasaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ganadorDeLaLigaToolStripMenuItem,
+            this.ganadorDeLaChampionsToolStripMenuItem});
+            this.regalitoDeLaCasaToolStripMenuItem.Name = "regalitoDeLaCasaToolStripMenuItem";
+            this.regalitoDeLaCasaToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.regalitoDeLaCasaToolStripMenuItem.Text = "Regalito de la casa";
+            this.regalitoDeLaCasaToolStripMenuItem.Click += new System.EventHandler(this.regalitoDeLaCasaToolStripMenuItem_Click);
+            // 
+            // ganadorDeLaLigaToolStripMenuItem
+            // 
+            this.ganadorDeLaLigaToolStripMenuItem.Name = "ganadorDeLaLigaToolStripMenuItem";
+            this.ganadorDeLaLigaToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.ganadorDeLaLigaToolStripMenuItem.Text = "ganador de la liga";
+            this.ganadorDeLaLigaToolStripMenuItem.Click += new System.EventHandler(this.ganadorDeLaLigaToolStripMenuItem_Click);
+            // 
+            // ganadorDeLaChampionsToolStripMenuItem
+            // 
+            this.ganadorDeLaChampionsToolStripMenuItem.Name = "ganadorDeLaChampionsToolStripMenuItem";
+            this.ganadorDeLaChampionsToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.ganadorDeLaChampionsToolStripMenuItem.Text = "ganador de la champions";
+            this.ganadorDeLaChampionsToolStripMenuItem.Click += new System.EventHandler(this.ganadorDeLaChampionsToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(657, 512);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProyectoGrupo7.Properties.Resources.cetys_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(545, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
             // Raul
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -67,11 +180,18 @@
             this.BackgroundImage = global::ProyectoGrupo7.Properties.Resources.fondo_pantalla_android;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(657, 536);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Raul";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = " ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +201,21 @@
 
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatosToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem regalitoDeLaCasaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ganadorDeLaLigaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ganadorDeLaChampionsToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
 
     }
 }
