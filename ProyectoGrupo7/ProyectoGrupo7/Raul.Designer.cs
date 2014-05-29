8dbd94c5070d9ccd84fa7502771d5f8c5b369e21
Luis Alvaro Sanz de Ayala Oliver <luisalvaro1239@gmail.com>
2014-05-29 20:00:30 +0200

    ??

--- ProyectoGrupo7/ProyectoGrupo7/Raul.Designer.cs
+++ ProyectoGrupo7/ProyectoGrupo7/Raul.Designer.cs
@@ -28,32 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reproductorDeMusicaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regalitoDeLaCasaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ganadorDeLaLigaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ganadorDeLaChampionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.coloresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // radioButton1
             // 
-            this.button1.Location = new System.Drawing.Point(21, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(301, 87);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "asi nos va";
-            this.button1.UseVisualStyleBackColor = true;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.Control;
+            this.radioButton1.Location = new System.Drawing.Point(107, 64);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(121, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ganador De La Liga";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.radioButton2.ForeColor = System.Drawing.SystemColors.Control;
+            this.radioButton2.Location = new System.Drawing.Point(405, 64);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(153, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ganador De La Champions";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.formatosToolStripMenuItem,
+            this.regalitoDeLaCasaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(657, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.cerrarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.cerrarToolStripMenuItem.Text = "Cerrar";
+            this.cerrarToolStripMenuItem.Click += new System.EventHandler(this.cerrarToolStripMenuItem_Click);
+            // 
+            // formatosToolStripMenuItem
+            // 
+            this.formatosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reproductorDeMusicaToolStripMenuItem,
+            this.coloresToolStripMenuItem});
+            this.formatosToolStripMenuItem.Name = "formatosToolStripMenuItem";
+            this.formatosToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.formatosToolStripMenuItem.Text = "Extras";
+            this.formatosToolStripMenuItem.Click += new System.EventHandler(this.formatosToolStripMenuItem_Click);
+            // 
+            // reproductorDeMusicaToolStripMenuItem
+            // 
+            this.reproductorDeMusicaToolStripMenuItem.Name = "reproductorDeMusicaToolStripMenuItem";
+            this.reproductorDeMusicaToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.reproductorDeMusicaToolStripMenuItem.Text = "Reproductor de musica";
+            this.reproductorDeMusicaToolStripMenuItem.Click += new System.EventHandler(this.reproductorDeMusicaToolStripMenuItem_Click);
+            // 
+            // regalitoDeLaCasaToolStripMenuItem
+            // 
+            this.regalitoDeLaCasaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ganadorDeLaLigaToolStripMenuItem,
+            this.ganadorDeLaChampionsToolStripMenuItem});
+            this.regalitoDeLaCasaToolStripMenuItem.Name = "regalitoDeLaCasaToolStripMenuItem";
+            this.regalitoDeLaCasaToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.regalitoDeLaCasaToolStripMenuItem.Text = "Regalito de la casa";
+            this.regalitoDeLaCasaToolStripMenuItem.Click += new System.EventHandler(this.regalitoDeLaCasaToolStripMenuItem_Click);
+            // 
+            // ganadorDeLaLigaToolStripMenuItem
+            // 
+            this.ganadorDeLaLigaToolStripMenuItem.Name = "ganadorDeLaLigaToolStripMenuItem";
+            this.ganadorDeLaLigaToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.ganadorDeLaLigaToolStripMenuItem.Text = "ganador de la liga";
+            this.ganadorDeLaLigaToolStripMenuItem.Click += new System.EventHandler(this.ganadorDeLaLigaToolStripMenuItem_Click);
+            // 
+            // ganadorDeLaChampionsToolStripMenuItem
+            // 
+            this.ganadorDeLaChampionsToolStripMenuItem.Name = "ganadorDeLaChampionsToolStripMenuItem";
+            this.ganadorDeLaChampionsToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.ganadorDeLaChampionsToolStripMenuItem.Text = "ganador de la champions";
+            this.ganadorDeLaChampionsToolStripMenuItem.Click += new System.EventHandler(this.ganadorDeLaChampionsToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(657, 512);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Color = System.Drawing.SystemColors.ControlText;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProyectoGrupo7.Properties.Resources.cetys_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(545, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // coloresToolStripMenuItem
+            // 
+            this.coloresToolStripMenuItem.Name = "coloresToolStripMenuItem";
+            this.coloresToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.coloresToolStripMenuItem.Text = "Colores";
+            this.coloresToolStripMenuItem.Click += new System.EventHandler(this.coloresToolStripMenuItem_Click);
             // 
             // Raul
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 340);
-            this.Controls.Add(this.button1);
+            this.BackgroundImage = global::ProyectoGrupo7.Properties.Resources.fondo_pantalla_android;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(657, 536);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Raul";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = " ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatosToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem regalitoDeLaCasaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ganadorDeLaLigaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ganadorDeLaChampionsToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem reproductorDeMusicaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem coloresToolStripMenuItem;
+
     }
 }
---
+++ ProyectoGrupo7/ProyectoGrupo7/reproductor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoGrupo7
+{
+    public partial class reproductor : Form
+    {
+        public reproductor()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            axWindowsMediaPlayer1.URL = openFileDialog1.FileName;
+        }
+
+        private void cerrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
