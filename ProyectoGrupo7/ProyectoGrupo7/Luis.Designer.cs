6f552fed451e613b21a33eef3d8fd1c4b87112c6
Luis Alvaro Sanz de Ayala Oliver <luisalvaro1239@gmail.com>
2014-05-29 19:56:22 +0200

    Vuelta verdadera(?)

--- ProyectoGrupo7/ProyectoGrupo7/Luis.Designer.cs
+++ ProyectoGrupo7/ProyectoGrupo7/Luis.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Snake
+﻿namespace ProyectoGrupo7
 {
     partial class Luis
     {
@@ -28,95 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+<<<<<<< HEAD
+=======
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+>>>>>>> origin/raul
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+<<<<<<< HEAD
+            this.button1.Location = new System.Drawing.Point(63, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+=======
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PruebaDeBotonGuardado";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(96, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+>>>>>>> origin/raul
             // 
             // Luis
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(546, 439);
+            this.ClientSize = new System.Drawing.Size(284, 262);
+<<<<<<< HEAD
+            this.Controls.Add(this.button1);
+            this.Name = "Luis";
+            this.Text = "0";
+=======
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Luis";
             this.Text = "Luis";
+>>>>>>> origin/raul
             this.ResumeLayout(false);
-
-            this.components = new System.ComponentModel.Container();
-            this.pbCanvas = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblScore = new System.Windows.Forms.Label();
-            this.gameTimer = new System.Windows.Forms.Timer(this.components);
-            this.lblGameOver = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pbCanvas
-            // 
-            this.pbCanvas.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.pbCanvas.Location = new System.Drawing.Point(13, 13);
-            this.pbCanvas.Name = "pbCanvas";
-            this.pbCanvas.Size = new System.Drawing.Size(432, 424);
-            this.pbCanvas.TabIndex = 0;
-            this.pbCanvas.TabStop = false;
-            this.pbCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pbCanvas_Paint);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(467, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 37);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Score: ";
-            // 
-            // lblScore
-            // 
-            this.lblScore.AutoSize = true;
-            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblScore.Location = new System.Drawing.Point(604, 13);
-            this.lblScore.Name = "lblScore";
-            this.lblScore.Size = new System.Drawing.Size(0, 37);
-            this.lblScore.TabIndex = 2;
-            // 
-            // lblGameOver
-            // 
-            this.lblGameOver.AutoSize = true;
-            this.lblGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGameOver.Location = new System.Drawing.Point(67, 45);
-            this.lblGameOver.Name = "lblGameOver";
-            this.lblGameOver.Size = new System.Drawing.Size(102, 37);
-            this.lblGameOver.TabIndex = 3;
-            this.lblGameOver.Text = "label2";
-            this.lblGameOver.Visible = false;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(744, 494);
-            this.Controls.Add(this.lblGameOver);
-            this.Controls.Add(this.lblScore);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pbCanvas);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        
-        private System.Windows.Forms.PictureBox pbCanvas;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblScore;
-        private System.Windows.Forms.Timer gameTimer;
-        private System.Windows.Forms.Label lblGameOver;
 
+        private System.Windows.Forms.Button button1;
+<<<<<<< HEAD
+=======
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+>>>>>>> origin/raul
     }
 }
-
-
-
